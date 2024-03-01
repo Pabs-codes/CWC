d5bb23210a6afc1248de5fb76b643db0e62738a2
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-03-01 15:53:24 +0530

    1st commit

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
+import Video from "@/components/Video";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ceylon web crafts",
+  description: "we empower your business with digital excellence",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+      <Blog />
+      <Contact />
+    </>
+  );
+}
